60f6f2c052e23c0911c5d5b71f25c4e1ea76fca3
lq0-github <1441665200@qq.com>
2024-04-11 18:01:07 +0800

    update Partners order

--- src/pages/homePage/homePartners.tsx
+++ src/pages/homePage/homePartners.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { 
-    PartnersIcon1, 
-    PartnersIcon2, 
-    PartnersIcon3, 
-    PartnersIcon4, 
-    PartnersIcon5, 
-    PartnersIcon6, 
-    PartnersIcon7, 
-    PartnersIcon8, 
-    PartnersIcon9 ,
-    PartnersIcon10, 
-    PartnersIcon11, 
-    PartnersIcon12, 
-    PartnersIcon13, 
+    PartnersRef, 
+    PartnersRainbow, 
+    PartnersFrax, 
+    PartnersImmunefi, 
+    PartnersNEAR, 
+    PartnersCedeStore, 
+    PartnersOKEX, 
+    PartnersPikespeak, 
+    PartnersCoin ,
+    PartnersSlowMist, 
+    PartnersPythOracle, 
+    PartnersBlocksec, 
+    PartnersSweatEconomy, 
 } from '../../components/partnersIcon';
 
 const HomePartners = () => {
@@ -21,82 +21,77 @@ const HomePartners = () => {
         <div className='t1a mb-8 md:mb-16 text-left'>PARTNERS</div>
         <div className='tablet-none mobile-none'>
           <div className="relative z-10 grid grid-cols-5 gap-y-16 w-full">
-            <PartnersIcon1 className="" />
-            <PartnersIcon2 className="justify-self-center self-center origin-center" />
-            <PartnersIcon3 className="justify-self-center self-center origin-center" />
-            <PartnersIcon4 className="justify-self-center self-center origin-center" />
-            <PartnersIcon5 className="justify-self-end self-end origin-end" />
+            <PartnersNEAR className="" />
+            <PartnersBlocksec className="justify-self-center self-center origin-center" />
+            <PartnersSlowMist className="justify-self-center self-center origin-center" />
+            <PartnersImmunefi className="justify-self-center self-center origin-center" />
+            <PartnersPythOracle className="justify-self-end self-end origin-end" />
           </div>
           <div className="relative z-10 grid grid-cols-5 gap-x-24 gap-y-16 mt-16 w-full">
-            <PartnersIcon6 className="" />
-            <PartnersIcon7 className="justify-self-center self-center origin-center" />
-            <PartnersIcon8 className="justify-self-center self-center origin-center" />
-            <PartnersIcon9 className="justify-self-center self-center origin-center" />
-            <PartnersIcon10 className="justify-self-end self-end origin-end" />
+            <PartnersRef className="" />
+            <PartnersOKEX className="justify-self-center self-center origin-center" />
+            <PartnersFrax className="justify-self-center self-center origin-center" />
+            <PartnersCoin className="justify-self-center self-center origin-center" />
+            <PartnersSweatEconomy className="justify-self-end self-end origin-end" />
           </div>
           <div className="relative z-10 grid grid-cols-5 gap-x-24 gap-y-16 mt-16 w-full">
-            <PartnersIcon11 className="" />
-            <PartnersIcon12 className="justify-self-center self-center origin-center" />
-            <PartnersIcon13 className="justify-self-center self-center origin-center" />
+            <PartnersPikespeak className="" />
+            <PartnersRainbow className="justify-self-center self-center origin-center" />
          </div>
         </div>
         <div className='mobile-none pc-none'>
          <div className="grid grid-cols-3 gap-x-20 gap-y-16">
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon1 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon2 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon3 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon4 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon5 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon6 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon7 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon8 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon9 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon10 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon11 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon12 /></div>
-             <div className="max-w-xs w-full flex justify-center items-center"><PartnersIcon13 /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersNEAR /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersBlocksec /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersSlowMist /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersImmunefi /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersPythOracle /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersRef /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersOKEX /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersFrax /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersCoin /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersSweatEconomy /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersPikespeak /></div>
+             <div className="max-w-xs w-full flex justify-center items-center"><PartnersRainbow /></div>
          </div>
         </div>
         <div className='pc-none tablet-none'>
         <div className="grid grid-cols-2 gap-x-4 gap-y-14 bg-gray-800 pl-8 pr-6 py-8 rounded-2xl">
             <div className="max-w-xs w-full flex justify-start items-center">
-              <PartnersIcon1 className="transform scale-90 -ml-3" />
+              <PartnersNEAR className="transform scale-90 -ml-3" />
             </div>
             <div className="max-w-xs w-full flex justify-end items-center">
-              <PartnersIcon2 className="transform scale-110 " />
+              <PartnersBlocksec className="transform scale-110 " />
             </div>
             <div className="max-w-xs w-full flex justify-start items-center">
-              <PartnersIcon3 className="transform scale-90 -ml-3" />
+              <PartnersSlowMist className="transform scale-90 -ml-4" />
             </div>
             <div className="max-w-xs w-full flex justify-end items-center">
-              <PartnersIcon4 className="transform scale-90 -mr-3" />
+              <PartnersImmunefi className="transform scale-90 -mr-3" />
             </div>
             <div className="max-w-xs w-full flex justify-start items-center">
-              <PartnersIcon6 className="transform scale-90 -ml-3 " />
+              <PartnersPythOracle className="transform scale-90 -ml-3 " />
             </div>
             <div className="max-w-xs w-full flex justify-end items-center">
-              <PartnersIcon5 className="origin-center -mr-1" />
+              <PartnersRef className="origin-center -mr-1" />
             </div>
             <div className="max-w-xs w-full flex justify-start items-center">
-              <PartnersIcon7 className="transform scale-90 -ml-3" />
+              <PartnersOKEX className="transform scale-90 -ml-3" />
             </div>
             <div className="max-w-xs w-full flex justify-end items-center">
-              <PartnersIcon8 className="transform scale-90 origin-center -mr-2.5" />
+              <PartnersFrax className="transform scale-90 origin-center -mr-2.5" />
             </div>
             <div className="max-w-xs w-full flex justify-start items-center">
-              <PartnersIcon10 className="transform scale-90 -ml-3" />
+              <PartnersCoin className="transform scale-90 -ml-3" />
             </div>
             <div className="max-w-xs w-full flex justify-end items-center">
-              <PartnersIcon11 className="transform scale-90 origin-center -mr-2" />
+              <PartnersSweatEconomy className="transform scale-90 origin-center -mr-2" />
             </div>
             <div className="max-w-xs w-full flex justify-start items-center">
-              <PartnersIcon9 className="transform scale-90 -ml-3" />
+              <PartnersPikespeak className="transform scale-90 -ml-3" />
             </div>
             <div className="max-w-xs w-full flex justify-end items-center">
-              <PartnersIcon12 className="transform scale-90 -ml-3 origin-center  -mr-3" />
-            </div>
-            <div className="max-w-xs w-full flex justify-start items-center">
-              <PartnersIcon13 className="transform scale-90 -ml-3" />
+              <PartnersRainbow className="transform scale-90 -ml-3 origin-center  -mr-3" />
             </div>
           </div>
         </div>
